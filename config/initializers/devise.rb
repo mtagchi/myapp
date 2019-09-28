3e23664d0efa6b66ce62d42b5bfa990fd35a2dc1
mtagchi <buscasnaz@gmail.com>
2019-09-28 08:40:27 +0000

    Finish Twitter signup

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,7 +260,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :twitter, ENV['API_KEY'], ENV['API_SECRET_KEY'], scope: 'email', callback_url: 'https://mysterious-temple-47807.herokuapp.com/users/auth/twitter/callback'
+  config.omniauth :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET'], scope: 'email', callback_url: 'https://protected-taiga-49884.herokuapp.com/users/auth/twitter'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
