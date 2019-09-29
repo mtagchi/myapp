ae8057c5b550f276825e278e7ca77ff4b8c3c1fd
mtagchi <buscasnaz@gmail.com>
2019-09-29 08:37:11 +0000

    Finish twitter signup

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,22 +1,21 @@
-# frozen_string_literal: true
-
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
-    callback_for(:twitter)
+    callback_from :twitter
   end
   
-  def callback_for(provider)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+  def callback_from(provider)
+    provider = provider.to_s
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
-      redirect_to new_user_registration_url
+      redirect_to root_path
     end
   end
 
-  def failure
-    redirect_to root_path
-  end
+  def after_sign_in_path_for(resource)
+    root_path
+ end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,26 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:twitter]
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name
-      user.image = auth.info.image
+         :recoverable, :rememberable, :validatable, :omniauthable
+
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+    unless user
+      user = User.create(
+        uid: auth.uid,
+        provider: auth.provider,
+        email: User.dummy_email(auth),
+        password: Devise.friendly_token[0, 20],
+        name: auth.info.name,
+        username: auth.info.nickname,
+        image: auth.info.image
+      )
     end
+    return user
+  end
+
+  private
+
+  def self.dummy_email(auth)
+   "#{auth.uid}-#{auth.provider}@example.com"
   end
 end
