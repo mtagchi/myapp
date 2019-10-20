123efa8116d6b1833f0f7cf3c2be0ce907e6073e
mtagchi <buscasnaz@gmail.com>
2019-10-20 22:35:32 +0000

    Delete unneeded migrations

--- db/migrate/20191005182322_allow_nil_name_to_tags.rb
+++
@@ -1,9 +0,0 @@
-class AllowNilNameToTags < ActiveRecord::Migration[6.0]
-  def up
-    change_column :tags, :name, :string, null: true
-  end
-
-  def down
-    change_column :tags, :name, :string, null: false
-  end
-end
