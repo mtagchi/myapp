08452f3f5b0ec1843200c0d13a3cbb3003fc032b
Zara Yakob <buscasnaz@gmail.com>
2020-02-21 00:43:02 +0900

    Set sign_in_redirect to events

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,6 +3,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     callback_from :twitter
   end
 
+  def after_sign_in_path_for(resource)
+    events_path
+  end
+
   def callback_from(provider)
     provider = provider.to_s
     @user = User.find_for_oauth(request.env['omniauth.auth'])
@@ -11,7 +15,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
-      redirect_to root_path
+      redirect_to events_path
     end
   end
 end
